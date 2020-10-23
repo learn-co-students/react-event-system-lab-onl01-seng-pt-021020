42681f545ab2ea062f0ca300090fb651988f0e41
samira-estes-garcia <samira.estes.garcia@gmail.com>
2020-10-22 23:08:13 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import { render } from 'enzyme'
+import React, {Component} from 'react'
+
+export default class Keypad extends Component {
+
+    onFocus = () => {console.log("Good!")}
+    onBlur = () => {console.log("Hey! Eyes on me!")}
+
+    render() {
+        return(
+            <button onFocus={this.onFocus} onBlur={this.onBlur}></button>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+import { render } from 'enzyme'
+import React, {Component} from 'react'
+
+export default class Keypad extends Component {
+
+    handleKeyUp = () => {
+        console.log("Entering password...")
+    }
+
+    render() {
+        return(
+            <input type="password" onKeyUp={this.handleKeyUp}/>
+        )
+    }
+}
